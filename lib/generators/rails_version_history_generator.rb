c6c065c3949b773e3c474fb8eeca6db39ea37196
Thorsten Muerell <thorsten@muerell.de>
2014-02-02 19:58:42 +0100

    Initial import

--- lib/generators/rails_version_history_generator.rb
+++ lib/generators/rails_version_history_generator.rb
@@ -1,10 +1,11 @@
 # encoding: utf-8
 require 'rails/generators/base'
 
-module RailsVersionHistory
-    class Generator < Rails::Generators::Base #:nodoc:
+class RailsVersionHistoryGenerator < Rails::Generators::NamedBase #:nodoc:
+      source_root File.expand_path("templates", File.dirname(__FILE__))
       argument :version_name, :type => :string, :default => 'versions', :banner => 'version_name'
 
+      desc "This initializer creates a version history"
       def create_config
         template "load_versions.rb", "config/initializers/load_#{file_name}.rb"
         copy_file "versions.yml", "config/#{file_name}.yml"
@@ -19,6 +20,4 @@ module RailsVersionHistory
       def constant_name
         version_name.underscore.upcase
       end
-
-    end
 end
